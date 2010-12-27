8680c0a2b1a88fa2cac1b2d36b2b5fa8a5e3b1b8
Rafael Lorandi <lorandi@ultraviolet.pisces.net>
2010-12-27 00:30:33 -0200

    Correção de bug na decisão da quantidade de novos valores de apostas.

--- comb_bet_step.sh
+++ comb_bet_step.sh
@@ -39,14 +39,14 @@ test $COMB_SIZE -gt 0 || {
 	exit 1
 }
 
-# Cycle through combinations, gathering the more valuables to form a bet template.
+# Cycle through combinations, gathering the most valuable ones to form a bet.
 COMB_VALUES_FILE=$(mktemp)
 NEW_COMB_VALUES_FILE=$(mktemp)
 for DISTINCT_SIZE in $(seq $COMB_SIZE -1 1) ; do
 	$UNZIP $COMBS_GZ_SOURCE_FILE | while read COMB ; do
 		HOW_MANY_VALUES_FOUND=$(wc -l 0<$COMB_VALUES_FILE)
 		HOW_MANY_VALUES_MISSING=$((HOW_MANY_WANTED_BET_VALUES-HOW_MANY_VALUES_FOUND))
-		test $HOW_MANY_VALUES_MISSING -gt 0 || break 2
+		test $HOW_MANY_VALUES_MISSING -ge $DISTINCT_SIZE || break
 		echo $COMB | unline 1>$NEW_COMB_VALUES_FILE
 		HOW_MANY_DISTINCT_VALUES=$(grep -v -F -x -f $COMB_VALUES_FILE $NEW_COMB_VALUES_FILE | wc -l)
 		test $HOW_MANY_DISTINCT_VALUES -ge $DISTINCT_SIZE || continue
